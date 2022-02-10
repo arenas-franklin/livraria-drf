1cbe39f540ba9cc5e726f0a76376fc9e2dc7df55
Franklin Angelo <angelod1@hotmail.com>
2022-02-10 15:01:01 -0300

    add configuracao das model no admin

--- core/models.py
+++ core/models.py
@@ -4,6 +4,12 @@ from django.db import models
 class Categoria(models.Model):
     descricao = models.CharField(max_length=255)
 
+    def __str__(self):
+        return self.descricao
+
 class Editora(models.Model):
     nome = models.CharField(max_length=255)
     site = models.URLField()
+
+    def __str__(self):
+        return self.nome
